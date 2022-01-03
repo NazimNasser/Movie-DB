411f083872a33b8efcc4f955123e02b842e3f8e5
Nazim Nasser <nazimnasser94@gmail.com>
2022-01-03 14:16:46 +0200

    step 3

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ const port = 3000
 
 app.get('/', (req, res)=> {
     res.send('ok')
+    
 })
+
+app.get('/test', (req, res)=> {
+    res.send({status:200, message:"ok"})
+})
+
+app.get('/time', (req, res)=> {
+    let time = new Date();
+    res.send({status:200, message:`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`})
+})
+
+app.listen(port)
